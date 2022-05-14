37a9abb4db5bc7992a08d29a5af7e8c71010d642
Hammad Ali <49644704+hammad11ali@users.noreply.github.com>
2022-05-14 16:27:54 +0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# OraclePLSQLUnwrapper
- VS Code extension for unwrapping Oracle PL/SQL Code 
+# PL/SQL Unwrapper 
+
+PL/SQL unwrapper is a simple extension for vs-code which let you unwrap the wrapped oracle pl/sql code.
+
+## Features
+
+This extension uses https://www.codecrete.net/UnwrapIt/ website to unwrap the code. The main feature of this extension to skip the need to copy the code to the website, unwrapping it and coppying it back to your editor. You can use `ctrl+alt+u` or click on unwrap option on status bar at the bottom of your editor to unwrap the code.
+
+Sample:
+
+![unwrap with status bar icon](gifs/unwrap-with-status-bar.gif)
+
+> Tip: You can use unwrap code command from command pallete or click on unwrap option in status bar or use `ctrl+alt+u` key to unwrap code. You can also change shortcut key in settings
+
+## Requirements
+
+None
+
+## Known Issues
+
+Unwrapping will through an error in case the wrapped code is corrupted. An active internet connection is required to unwrap the code.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of PL/SQL unwrapper
+
+## Report an issue
+You can raise issues on https://github.com/hammad11ali/PLSQL-Unwrapper in case you find any bugs in the extension.
+
+-----------------------------------------------------------------------------------------------------------
+
+**Note:** Special thanks to the creator of https://www.codecrete.net/UnwrapIt/ which made the creation of this extension possible
+
---
+++ extension.js
@@ -0,0 +1,91 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const axios = require('axios');
+var FormData = require('form-data');
+const html_parser = require('node-html-parser');
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+
+let myStatusBarItem = vscode.StatusBarItem;
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "unwrapper" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('unwrapper.unwrap', function () {
+		// The code you place here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			updateStatusBar('Loading');
+			vscode.window.showInformationMessage('Unwrapping your code!');
+			const selection = editor.selection;
+			const text = editor.document.getText(selection);
+			var data = new FormData();
+			data.append('Code', text);
+			data.append('UnwrapText', 'Unwrap Code');
+
+			var config = {
+				method: 'post',
+				url: 'https://www.codecrete.net/UnwrapIt/',
+				headers: {
+					...data.getHeaders()
+				},
+				data: data
+			};
+
+			axios(config)
+				.then(function (response) {
+					console.log(JSON.stringify(response.data));
+					const res = response.data;
+					const root = html_parser.parse(res);
+					const text = root.querySelector('pre').text;
+					editor.edit(editBuilder => {
+						editBuilder.replace(selection, text);
+					});
+					vscode.window.showInformationMessage('Unwrapping completed!');
+					updateStatusBar('Done');
+				})
+				.catch(function (error) {
+					vscode.window.showErrorMessage('Unwrapper failed!');
+					console.log(error);
+					updateStatusBar('Done');
+				});
+		}
+	});
+
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.left, 1);
+	myStatusBarItem.command = 'unwrapper.unwrap';
+	context.subscriptions.push(myStatusBarItem);
+	context.subscriptions.push(disposable);
+	updateStatusBar('Ready');
+}
+
+
+function updateStatusBar(status) {
+	if (status === 'Loading') {
+		myStatusBarItem.text = '$(sync~spin) Unwrapping...';
+		myStatusBarItem.show();
+	}
+	else {
+		myStatusBarItem.text = '$(play) Unwrap Code';
+		myStatusBarItem.show();
+	}
+}
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
